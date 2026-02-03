e2b0d8a7dcae1fe91ba83f091311505b0870b638
Evgeny Zislis <evgeny.zislis@gmail.com>
2026-02-03 23:27:41 +0200

    Simplify arion config to use existing Dockerfile

--- arion-compose.nix
+++ arion-compose.nix
@@ -3,68 +3,27 @@
   project.name = "ibkr";
   
   services.tws = {
-    nixos.useSystemd = true;
-    nixos.configuration = { config, pkgs, ... }: {
-      
-      # Install packages
-      environment.systemPackages = with pkgs; [
-        openjdk21
-        javaPackages.openjfx21
-        curl
-        unzip
-        xorg.xorgserver
-        xorg.xauth
-        xvfb-run
-        x11vnc
-        openbox
-        tint2
-        bash
-        coreutils
-        findutils
-        gnused
-        gnugrep
-        util-linux
-      ];
-      
-      # Create tws user
-      users.users.tws = {
-        isNormalUser = true;
-        uid = 1000;
-        home = "/home/tws";
-      };
-      
-      # Environment
-      environment.variables = {
-        DISPLAY = ":0";
-        TZ = "Asia/Jerusalem";
-        FONTCONFIG_FILE = "/etc/fonts/fonts.conf";
-      };
-      
-      # TWS service
-      systemd.services.ibkr-tws = {
-        description = "IBKR TWS with IBC";
-        wantedBy = [ "multi-user.target" ];
-        after = [ "network.target" ];
-        
-        serviceConfig = {
-          Type = "simple";
-          User = "tws";
-          WorkingDirectory = "/home/tws";
-          Restart = "always";
-          RestartSec = "10s";
-        };
-        
-        script = ''
-          # Will add the full startup script here
-          sleep infinity
-        '';
-      };
-    };
-    
-    service.useHostStore = true;
+    service.build.context = "./docker";
     service.ports = [ "5901:5900" ];
     service.volumes = [
       "tws-data:/home/tws/jts"
     ];
+    service.secrets = [{
+      source = "tws";
+      target = "/run/secrets/tws";
+    }];
+    service.restart = "unless-stopped";
+    service.environment = {
+      DISPLAY = ":0";
+      TZ = "Asia/Jerusalem";
+    };
+  };
+  
+  docker-compose.raw.secrets = {
+    tws.file = "./docker/tws.secrets";
+  };
+  
+  docker-compose.raw.volumes = {
+    tws-data = {};
   };
 }
